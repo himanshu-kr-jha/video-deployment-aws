7650bafd029673691674ec94e5f22d30b2b42f86
Himanshu Kumar Jha <himanshukrjha004@gmail.com>
2024-06-21 14:15:00 +0530

    created function and made req.txt file

---
+++ process_video.py
@@ -0,0 +1,9 @@
+import os
+import logging
+import click
+import cv2
+import boto3
+from botocore.exceptions import ClientError 
+
+def movement_detection(input_video_path,output_video_path):
+    pass
